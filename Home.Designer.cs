343b45af1fe24ca3ef980774dca20634e06acc8b
CN-Root <admin@root.tax>
2022-08-10 14:41:35 +0800

    project file

---
+++ Home.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace Procedures_to_protect
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.PS1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.information4 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.Label();
+            this.information3 = new System.Windows.Forms.Label();
+            this.information2 = new System.Windows.Forms.Label();
+            this.information1 = new System.Windows.Forms.Label();
+            this.process_ps3 = new System.Windows.Forms.Label();
+            this.process_ps1 = new System.Windows.Forms.Label();
+            this.process_ps2 = new System.Windows.Forms.Label();
+            this.PS2 = new System.Windows.Forms.Label();
+            this.guardian = new System.Windows.Forms.Timer(this.components);
+            this.PS3 = new System.Windows.Forms.Label();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PS1
+            // 
+            this.PS1.AutoSize = true;
+            this.PS1.Location = new System.Drawing.Point(9, 6);
+            this.PS1.Name = "PS1";
+            this.PS1.Size = new System.Drawing.Size(200, 17);
+            this.PS1.TabIndex = 0;
+            this.PS1.Text = "使用前请先手动开打需要保护的进程";
+            // 
+            // textBox
+            // 
+            this.textBox.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox.Location = new System.Drawing.Point(12, 43);
+            this.textBox.Name = "textBox";
+            this.textBox.ReadOnly = true;
+            this.textBox.Size = new System.Drawing.Size(281, 23);
+            this.textBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(299, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.information4);
+            this.groupBox.Controls.Add(this.number);
+            this.groupBox.Controls.Add(this.information3);
+            this.groupBox.Controls.Add(this.information2);
+            this.groupBox.Controls.Add(this.information1);
+            this.groupBox.Controls.Add(this.process_ps3);
+            this.groupBox.Controls.Add(this.process_ps1);
+            this.groupBox.Controls.Add(this.process_ps2);
+            this.groupBox.Location = new System.Drawing.Point(12, 68);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(360, 91);
+            this.groupBox.TabIndex = 3;
+            this.groupBox.TabStop = false;
+            // 
+            // information4
+            // 
+            this.information4.AutoSize = true;
+            this.information4.Location = new System.Drawing.Point(66, 70);
+            this.information4.Name = "information4";
+            this.information4.Size = new System.Drawing.Size(39, 17);
+            this.information4.TabIndex = 10;
+            this.information4.Text = "NULL";
+            // 
+            // number
+            // 
+            this.number.AutoSize = true;
+            this.number.Location = new System.Drawing.Point(6, 70);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(68, 17);
+            this.number.TabIndex = 5;
+            this.number.Text = "守护次数：";
+            // 
+            // information3
+            // 
+            this.information3.AutoSize = true;
+            this.information3.Location = new System.Drawing.Point(66, 53);
+            this.information3.Name = "information3";
+            this.information3.Size = new System.Drawing.Size(39, 17);
+            this.information3.TabIndex = 9;
+            this.information3.Text = "NULL";
+            // 
+            // information2
+            // 
+            this.information2.AutoSize = true;
+            this.information2.Location = new System.Drawing.Point(66, 36);
+            this.information2.Name = "information2";
+            this.information2.Size = new System.Drawing.Size(39, 17);
+            this.information2.TabIndex = 8;
+            this.information2.Text = "NULL";
+            // 
+            // information1
+            // 
+            this.information1.AutoSize = true;
+            this.information1.Location = new System.Drawing.Point(66, 19);
+            this.information1.Name = "information1";
+            this.information1.Size = new System.Drawing.Size(39, 17);
+            this.information1.TabIndex = 7;
+            this.information1.Text = "NULL";
+            // 
+            // process_ps3
+            // 
+            this.process_ps3.AutoSize = true;
+            this.process_ps3.Location = new System.Drawing.Point(6, 53);
+            this.process_ps3.Name = "process_ps3";
+            this.process_ps3.Size = new System.Drawing.Size(68, 17);
+            this.process_ps3.TabIndex = 6;
+            this.process_ps3.Text = "进程路径：";
+            // 
+            // process_ps1
+            // 
+            this.process_ps1.AutoSize = true;
+            this.process_ps1.Location = new System.Drawing.Point(6, 19);
+            this.process_ps1.Name = "process_ps1";
+            this.process_ps1.Size = new System.Drawing.Size(64, 17);
+            this.process_ps1.TabIndex = 5;
+            this.process_ps1.Text = "进程PID：";
+            // 
+            // process_ps2
+            // 
+            this.process_ps2.AutoSize = true;
+            this.process_ps2.Location = new System.Drawing.Point(6, 36);
+            this.process_ps2.Name = "process_ps2";
+            this.process_ps2.Size = new System.Drawing.Size(68, 17);
+            this.process_ps2.TabIndex = 4;
+            this.process_ps2.Text = "进程名称：";
+            // 
+            // PS2
+            // 
+            this.PS2.AutoSize = true;
+            this.PS2.Location = new System.Drawing.Point(9, 24);
+            this.PS2.Name = "PS2";
+            this.PS2.Size = new System.Drawing.Size(297, 17);
+            this.PS2.TabIndex = 4;
+            this.PS2.Text = "请填写需要保护的进程文件名(不需要填写<.exe>后缀)";
+            // 
+            // guardian
+            // 
+            this.guardian.Tick += new System.EventHandler(this.guardian_Tick);
+            // 
+            // PS3
+            // 
+            this.PS3.AutoSize = true;
+            this.PS3.Location = new System.Drawing.Point(246, 6);
+            this.PS3.Name = "PS3";
+            this.PS3.Size = new System.Drawing.Size(135, 17);
+            this.PS3.TabIndex = 5;
+            this.PS3.Text = "INS键 - 隐藏/显示 窗体";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.PS3);
+            this.Controls.Add(this.PS2);
+            this.Controls.Add(this.groupBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.PS1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "进程守护";
+            this.Load += new System.EventHandler(this.Home_Load);
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PS1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Label process_ps2;
+        private System.Windows.Forms.Label process_ps1;
+        private System.Windows.Forms.Label process_ps3;
+        private System.Windows.Forms.Label information3;
+        private System.Windows.Forms.Label information2;
+        private System.Windows.Forms.Label information1;
+        private System.Windows.Forms.Label PS2;
+        private System.Windows.Forms.Timer guardian;
+        private System.Windows.Forms.Label number;
+        private System.Windows.Forms.Label information4;
+        private System.Windows.Forms.Label PS3;
+    }
+}
---
+++ Home.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Net;//其下有WebRequest和WebResponse
+using System.Text.RegularExpressions;//正则表达式，比截取字符串要好用的多
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Threading;
+using System.Resources;
+using System.Reflection;
+using System.Xml;
+using System.Management;
+using System.Runtime;
+using System.Net.Mail;
+using System.Data.SqlClient;
+
+namespace Procedures_to_protect
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         *  廊坊纽扣网络技术有限公司
+         */
+
+        #region A1
+        string wendang = (Environment.GetFolderPath(Environment.SpecialFolder.Personal));//判断电脑文档路径
+
+        string zhuomian = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);//判断电脑桌面路径
+
+        string Time = DateTime.Now.ToLocalTime().ToString();//检测当前电脑时间
+
+        [DllImport("user32.dll", EntryPoint = "AnimateWindow")]
+        private static extern bool AnimateWindow(IntPtr handle, int ms, int flags);
+
+        [DllImport("shell32.dll")]
+        public static extern int ShellExecute(IntPtr hwnd, StringBuilder lpszOp, StringBuilder lpszFile, StringBuilder lpszParams, StringBuilder lpszDir, int FsShowCmd);
+        #endregion
+
+        #region 快捷键
+        public class HotKey
+        {
+            //如果函数执行成功，返回值不为0。
+            //如果函数执行失败，返回值为0。要得到扩展错误信息，调用GetLastError。
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool RegisterHotKey(
+                IntPtr hWnd,                //要定义热键的窗口的句柄
+                int id,                     //定义热键ID（不能与其它ID重复）           
+                KeyModifiers fsModifiers,   //标识热键是否在按Alt、Ctrl、Shift、Windows等键时才会生效
+                Keys vk                     //定义热键的内容
+                );
+
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern bool UnregisterHotKey(
+                IntPtr hWnd,                //要取消热键的窗口的句柄
+                int id                      //要取消热键的ID
+                );
+
+            //定义了辅助键的名称（将数字转变为字符以便于记忆，也可去除此枚举而直接使用数值）
+            [Flags()]
+            public enum KeyModifiers
+            {
+                None = 0,
+                Alt = 1,
+                Ctrl = 2,
+                Shift = 4,
+                WindowsKey = 8
+            }
+
+        }
+        #endregion
+
+        #region 加载主页
+        private void Home_Load(object sender, EventArgs e)
+        {
+            HotKey.RegisterHotKey(Handle, 103, HotKey.KeyModifiers.None, Keys.Insert);//注册热键INS,ID为103
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键    
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+                        case 103:
+                            buttonbox();
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        string Hidden_display = "真";
+        void buttonbox() 
+        {
+            if (Hidden_display == "真")
+            {
+                this.Hide();
+                Hidden_display = "假";
+            }
+            else 
+            {
+                this.Show();
+                Hidden_display = "真";
+            }
+        }
+        #endregion
+
+        #region 选择路径
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PS1.Focus();//重设焦点
+
+            if (button1.Text == "查询")
+            {
+                #region 选取文件
+                //此时弹出一个可以选择文件的窗体
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                //一次只能选取一个文件
+                fileDialog.Multiselect = false;
+                fileDialog.Title = "请选择需要保护的进程源文件";
+                fileDialog.Filter = "所有文件(*.exe*)|*.exe*";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    textBox.Text = fileDialog.FileName;
+                }
+                #endregion
+
+                #region 获取文件名
+                string strFilePaht = fileDialog.FileName;
+                string str = Path.GetFileNameWithoutExtension(strFilePaht);
+                #endregion
+
+                #region 获取进程信息
+                if (System.Diagnostics.Process.GetProcessesByName(str).ToList().Count > 0)
+                {//运行中
+                    Process[] ps = Process.GetProcesses();//获取计算机上所有进程
+                    foreach (Process p in ps)
+                    {
+                        if (p.ProcessName == str)//判断进程名称
+                        {
+                            string a = p.Id.ToString();
+
+                            information1.Text = a;
+
+                            information2.Text = p.ProcessName;
+
+                            information3.Text = fileDialog.FileName;
+                        }
+                    }
+
+                    DialogResult result = MessageBox.Show("是否开启保护" + "\n\n" + "点击<是>开始保护" + "\n" + "点击<否>取消操作", "是否开启进程保护？", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                    if (result == DialogResult.Yes)
+                    {
+                        string strFilePaht2 = textBox.Text;
+                        string str2 = Path.GetFileNameWithoutExtension(strFilePaht2);
+
+                        guardian.Enabled = true;//开启计时器
+                    }
+                    else if (result == DialogResult.No)
+                    {
+                        textBox.Text = "";
+                        information1.Text = "NULL";
+                        information2.Text = "NULL";
+                        information3.Text = "NULL";
+                    }
+                }
+                else
+                {
+                    textBox.Text = "";
+                    information1.Text = "NULL";
+                    information2.Text = "NULL";
+                    information3.Text = "NULL";
+                    information4.Text = "NULL";
+
+                    MessageBox.Show("可能是<文件名>输入有误", "未检测到需要保护的进程");//信息提示
+                }
+                #endregion
+            }
+
+            #region 结束保护
+            if (button1.Text == "结束")
+            {
+                textBox.Text = "";
+                information1.Text = "NULL";
+                information2.Text = "NULL";
+                information3.Text = "NULL";
+                information4.Text = "NULL";
+
+                button1.Text = "查询";
+                guardian.Enabled = false;
+                MessageBox.Show("已结束进程保护", "提示：");
+            }
+            #endregion
+        }
+        #endregion
+
+        #region 开始保护进程(计时器)
+        int record1 = 1;//记录值
+        private void guardian_Tick(object sender, EventArgs e)
+        {//开始保护进程
+            if (button1.Text == "查询")
+            {
+                button1.Text = "结束";
+
+                string strFilePaht2 = textBox.Text;
+                string str2 = Path.GetFileNameWithoutExtension(strFilePaht2);
+
+                information4.Text = "0";
+
+                MessageBox.Show("进程保护已开启" + "\n\n" + "保护的进程：[ " + str2 + " ]", "提示：");
+            }
+
+            #region 获取文件名
+            string strFilePaht = textBox.Text;
+            string str = Path.GetFileNameWithoutExtension(strFilePaht);
+            #endregion
+
+            if (System.Diagnostics.Process.GetProcessesByName(str).ToList().Count > 0)
+            {//运行中
+
+            }
+            else
+            {//未运行
+                if (!File.Exists(@strFilePaht))//检测系统是否通过检测
+                {//不存在文件
+                    guardian.Enabled = false;//进程保护结束
+
+                    textBox.Text = "";
+                    information1.Text = "NULL";
+                    information2.Text = "NULL";
+                    information3.Text = "NULL";
+                    information4.Text = "NULL";
+
+                    button1.Text = "查询";
+
+                    MessageBox.Show("进程保护已结束！！！" 
+                        + "\n\n" 
+                        + "原因：" 
+                        + "\n" 
+                        + "1.文件移动" 
+                        + "\n" 
+                        + "2.文件删除"
+                        +"\n"
+                        +"3.文件重命名"
+                        ,"提示：");
+                }
+                else
+                {//存在文件
+                    System.Diagnostics.Process.Start(@strFilePaht);//启动外部程序
+
+                    float flNum1 = Convert.ToSingle(information4.Text);
+                    float flNum2 = Convert.ToSingle(record1);
+                    float sum = flNum1 + flNum2;
+                    information4.Text = Convert.ToString(sum);
+
+                    Process[] ps = Process.GetProcesses();//获取计算机上所有进程
+                    foreach (Process p in ps)
+                    {
+                        if (p.ProcessName == str)//判断进程名称
+                        {
+                            string a = p.Id.ToString();
+
+                            information1.Text = a;
+                        }
+                    }
+                }
+            }
+        }
+        #endregion
+    }
+}
